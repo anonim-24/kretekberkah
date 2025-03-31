2b11f6078c45edd48dcb6b26eb7d94c38eaa396c
anonim-24 <akbaralfa024@gmail.com>
2025-03-31 09:06:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contoh script untuk interaksi di website, jika diperlukan.
+// Seperti animasi atau validasi form.
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    alert("Form berhasil dikirim!");
+});
